be955ef89f9c8fa8958da6c09d687ea6f5c9b20b
Kenan Curevac <kenancurevac@gmail.com>
2026-04-06 22:55:07 +0200

    allowing to fetch from cors

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://pin-the-place-kenan.vercel.app/",
+    origin: true,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
