562289e0fe6d2561e7ac2bd11ce65e89f73b6779
Kenan Curevac <kenancurevac@gmail.com>
2026-04-06 22:46:52 +0200

    setup for vercel

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,29 @@ import "dotenv/config";
 import express from "express";
 import router from "./routes/routes.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
+import cors from "cors";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "https://pin-the-place-kenan.vercel.app/",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
 app.use(express.json());
 app.use("/api", router);
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 3000;
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`🚀 Server running locally on http://localhost:${PORT}`);
+  });
+}
 
-app.listen(PORT, () => {
-  console.log(`Running on Port ${PORT}`);
-});
+export default app;
