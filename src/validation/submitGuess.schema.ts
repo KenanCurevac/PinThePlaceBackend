f09869454e7b2562b2ace826bea6188a644ba318
Kenan Curevac <kenancurevac@gmail.com>
2026-04-26 20:13:45 +0200

    zod allowing null for guess added

--- src/validation/submitGuess.schema.ts
+++ src/validation/submitGuess.schema.ts
@@ -2,6 +2,6 @@ import { z } from "zod";
 
 export const submitGuessSchema = z.object({
   questionId: z.string().cuid(),
-  guessLat: z.coerce.number(),
-  guessLng: z.coerce.number(),
+  guessLat: z.number().nullable(),
+  guessLng: z.number().nullable(),
 });
